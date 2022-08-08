2b6720aa481e7aa415d032d0674b0e238ee1042f
unknown <ing.luischavez.dev>
2022-08-08 14:01:15 -0500

    Navbar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,18 @@
+import { Navbar } from "../"
 
 
 export const CalendarPage = () => {
   return (
-    <div>CalendarPage</div>
+    <div className="navbar navbar-dark bg-dark mb-4 px-4">
+      <span className="navbar-brand">
+        <i className="fas fa-calendar-alt mr-2"></i>
+        <span>Hansi Flick</span>
+      </span>
+
+      <button className="btn btn-outline-danger">
+        <i className="fas fa-sign-out-alt mr-2"></i>
+        <span>Salir</span>
+      </button>
+    </div>
   )
 }
