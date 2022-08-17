3047733c3f056e245d7a16eccd4eb6555ce1f2b5
unknown <ing.luischavez.dev>
2022-08-17 07:47:50 -0500

    Calendario

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -2,6 +2,16 @@
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar navbar-dark bg-dark mb-4 px-4">
+      <span className="navbar-brand">
+        <i className="fas fa-calendar-alt mr-2"></i>
+        <span>Hansi Flick</span>
+      </span>
+
+      <button className="btn btn-outline-danger">
+        <i className="fas fa-sign-out-alt mr-2"></i>
+        <span>Salir</span>
+      </button>
+    </div>
   )
 }
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,18 +1,48 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
+import { enUS } from 'date-fns/locale'
+
+// components
 import { Navbar } from "../"
 
+const locales = {
+  'en-US': enUS,
+}
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [
+  {
+    title: 'Cumpleaños de mi pana',
+    notes: 'Comprar cartón de chelas',
+    start: new Date(),
+    end: addHours( new Date(), 2 ),
+    bgColor: '#fafafa',
+    user: {
+      id: '123',
+      name: 'Alberto'
+    }
+  }
+]
 
 export const CalendarPage = () => {
   return (
-    <div className="navbar navbar-dark bg-dark mb-4 px-4">
-      <span className="navbar-brand">
-        <i className="fas fa-calendar-alt mr-2"></i>
-        <span>Hansi Flick</span>
-      </span>
-
-      <button className="btn btn-outline-danger">
-        <i className="fas fa-sign-out-alt mr-2"></i>
-        <span>Salir</span>
-      </button>
-    </div>
+    <>
+      <Navbar />
+      
+      <Calendar
+        localizer={ localizer }
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+      />
+    </>
   )
 }
